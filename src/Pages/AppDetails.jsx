3eaaaefccb370ff4147299df7c9a545b98115580
Md Abidur Rahman <abid19918february@gmail.com>
2025-10-15 15:24:12 +0600

    React Router setup is completed

---
+++ src/Pages/AppDetails.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AppDetails = () => {
+    return (
+        <div>
+            <h1>Apps Details Page</h1> 
+        </div>
+    );
+};
+
+export default AppDetails;
+
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,29 @@
+import { createBrowserRouter } from "react-router";
+import Home from "../Pages/Home";
+import Products from "../Pages/Products";
+import MainLayout from "../Layouts/MainLayout";
+import ErrorPage from "../Pages/ErrorPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index:true,
+        element: <Home />,
+      },
+      {
+        path: "/products",
+        element: <Products />,
+      }
+    ]
+  },
+  {
+    path:'*',
+    element: <ErrorPage />,
+  }
+]);
+
+export default router;
